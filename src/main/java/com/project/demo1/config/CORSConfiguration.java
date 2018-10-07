f7acd94249df661829f7f0e6b3fcdfb6502314be
HelloWorld <1036499605@qq.com>
2018-10-07 14:31:43 +1100

    CORSConfiguration - done

---
+++ src/main/java/com/project/demo1/config/CORSConfiguration.java
@@ -0,0 +1,18 @@
+package com.project.demo1.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class CORSConfiguration extends WebMvcConfigurerAdapter {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("*")
+                .allowedHeaders("*");
+        super.addCorsMappings(registry);
+    }
+}
+
